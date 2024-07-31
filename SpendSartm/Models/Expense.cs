3c09ef05602dec59b6296968f8cd060d99af4d25
senzonzuza <senzokaqhaka@gmail.com>
2024-07-31 11:52:06 +0200

    Expense model created

---
+++ SpendSartm/Models/Expense.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SpendSartm.Models
+{
+	public class Expense
+	{
+		public int Id { get; set; }
+		public decimal Value { get; set; }
+		[Required]
+		public string?  Decription { get; set; }
+	}
+}
+
